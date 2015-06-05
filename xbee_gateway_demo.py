3cccfc405a37edde7aa3cab45c979716431ee6a4
Jan Bogaerts <jb@allthingstalk.com>
2015-06-05 10:40:05 +0200

    -changes name of library -changes library to full gateway functionality

--- xbee_gateway_demo.py
+++ xbee_gateway_demo.py
@@ -4,7 +4,7 @@
 
 import serial
 from xbee import ZigBee
-import allthingstalk_python_gateway_lib as IOT                              #provide cloud support
+import ATT_IOT_Gateway as IOT                              #provide cloud support
 
 #set up the ATT internet of things platform
 IOT.ClientId = "YourClientId"
