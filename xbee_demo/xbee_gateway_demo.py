17dbb98efcf24dfa58f7bb05320fce831c2a19ab
Jan Bogaerts <jb@allthingstalk.com>
2015-08-11 17:53:29 +0200

    updates xbee demo

--- xbee_demo/xbee_gateway_demo.py
+++ xbee_demo/xbee_gateway_demo.py
@@ -3,12 +3,10 @@
 # not yet supported
 
 import serial
-from xbee import ZigBee
 from time import sleep                             #pause the app
-import logging
 from ConfigParser import *
 from uuid import getnode as get_mac
-
+from xbee import ZigBee
 import att_iot_gateway as IOT                              #provide cloud support
 
 def connect():
@@ -29,9 +27,9 @@ def authenticate(maxClaim = 30):
     '''
     if _tryLoadConfig() == False:
         uid = _getUid();
-        IOT.createGateway("Liato gateway", uid)
+        IOT.createGateway("xbee gateway", uid)
         while True:                                     # we try to finish the claim process until success or app quits, cause the app can't continue without a valid and claimed gateway
-            if IOT.finishclaim("Liato gateway", uid):
+            if IOT.finishclaim("xbee gateway", uid):
                 _storeConfig()
                 sleep(2)                                # give the platform a litle time to set up all the configs so that we can subscribe correctly to the topic. If we don't do this, the subscribe could fail
                 return True
@@ -40,11 +38,33 @@ def authenticate(maxClaim = 30):
         return False                                # if we get here, didn't succeed in time to finish the claiming process.
     else:
         if IOT.authenticate():
-            logging.info('Authenticated')
+            print('Authenticated')
             return True
         else:
-            logging.error('failed to authenticate')
+            print('failed to authenticate')
             return False
+
+def _tryLoadConfig():
+    'load the config from file'
+    c = ConfigParser()
+    if c.read('gateway.config'):
+        IOT.GatewayId = c.get('general', 'gatewayId')
+        IOT.ClientId = c.get('general', 'clientId')
+        IOT.ClientKey = c.get('general', 'clientKey')
+        return True
+    else:
+        return False
+
+def _storeConfig():
+    "store the current gateway settings in a config file"
+    c = ConfigParser()
+    c.add_section('general')
+    c.set('general', 'gatewayId', IOT.GatewayId)
+    c.set('general', 'clientId', IOT.ClientId)
+    c.set('general', 'clientKey', IOT.ClientKey)
+
+    with open('gateway.config', 'w') as f:
+        c.write(f)
 
 def _getUid():
     'extract the mac address in order to identify the gateway in the cloud'
@@ -59,43 +79,28 @@ def _getUid():
     while len(result) < 12:                                                         # it could be that there were missing '0' in the front, add them manually.
         result = "0" + result
     result = result.upper()                                                         # make certain that it is upper case, easier to read, more standardized
-    logging.info('mac address: ' + result)
+    print('mac address: ' + result)
     return result
 
 
-def _storeConfig():
-    "store the current gateway settings in a config file"
-    c = ConfigParser()
-    c.add_section('general')
-    c.set('general', 'gatewayId', IOT.GatewayId)
-    c.set('general', 'clientId', IOT.ClientId)
-    c.set('general', 'clientKey', IOT.ClientKey)
 
-    with open('../config/gateway.config', 'w') as f:
-        c.write(f)
-
-def _tryLoadConfig():
-    'load the config from file'
-    c = ConfigParser()
-    if c.read('../config/gateway.config'):
-        IOT.GatewayId = c.get('general', 'gatewayId')
-        IOT.ClientId = c.get('general', 'clientId')
-        IOT.ClientKey = c.get('general', 'clientKey')
-        return True
-    else:
-        return False
 
 #serial_port = serial.Serial('/dev/ttyUSB0', 9600)        #for linux
-serial_port = serial.Serial('COM5', 9600)                 #for windows
+serial_port = serial.Serial('COM3', 9600)                 #for windows
 zb = ZigBee(serial_port)
 
 #callback: handles values sent from the cloudapp to the device
-def on_message(deviceId, assetId, value):
-    print "not yet supported"
+def on_message(device, actuator, value):
+    if actuator == "1":
+        if value == "true":
+            #note: best to store the destination address (long and short) from the incomming packets, so it becomes dynamic
+            zb.send("remote_at", frame="A", command="D4", dest_addr_long = '\x00\x13\xa2\x00@w\x0f\xd2', dest_addr="QH",parameter='\x05')        # should turn on relay
+        else:
+            zb.send("remote_at", frame="A", command="D4", dest_addr_long = '\x00\x13\xa2\x00@w\x0f\xd2', dest_addr="QH",parameter='\x04')        # should turn off relay
 
-#make certain that the device & it's features are defined in the cloudapp
 
 devices = []                                           #contains the list of devices already connected to the
+
 
 connect()
 while True:
@@ -110,12 +115,10 @@ while True:
             if IOT.deviceExists(deviceId) == False:     										#as we only keep deviceId's locally in memory, it could be that we already created the device in a previous run. We only want to create it 1 time.
                 print "creating new device"
                 IOT.addDevice(deviceId, 'name of the device', "description of the device" )		#adjust according to your needs
-                IOT.addAsset(1, deviceId, 'asset name', 'asset description', False, 'int')	#adjust according to your needs
-                IOT.addAsset(2, deviceId, "asset name", "asset description", False, "int")	#adjust according to your needs
-                IOT.addAsset(3, deviceId, "asset name", "asset description", False, "int")	#adjust according to your needs
-        IOT.send(data['samples'][0]['adc-1'], deviceId, 1)									#adjust according to your needs
-        IOT.send(data['samples'][0]['adc-2'], deviceId, 2)									#adjust according to your needs
-        IOT.send(data['samples'][0]['adc-3'], deviceId, 3)									#adjust according to your needs
+                IOT.addAsset(1, deviceId, 'relay', 'switch something on/off', True, 'boolean')	#adjust according to your needs
+        # IOT.send(data['samples'][0]['adc-1'], deviceId, 1)									#adjust according to your needs
+        # IOT.send(data['samples'][0]['adc-2'], deviceId, 2)									#adjust according to your needs
+        # IOT.send(data['samples'][0]['adc-3'], deviceId, 3)									#adjust according to your needs
     except KeyboardInterrupt:                                                    				#stop the script
         break
     except ValueError as e:                                                      				#in case of an xbee error: print it and try to continue
